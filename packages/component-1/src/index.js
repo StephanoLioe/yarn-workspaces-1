ef23f81b9c8a466a3d7a3f986ce9b703d59cc85d
Stephano Lioe <stephano.lioe@iodigital.com>
2024-03-08 16:34:41 +0100

    working with js files

---
+++ packages/component-1/src/index.js
@@ -0,0 +1,5 @@
+export const component1 = () => {
+  console.log("component1");
+};
+
+export const name = "Hallo allemaal!";
---
+++ packages/component-2/src/index.js
@@ -0,0 +1,6 @@
+import { name } from "@company/component-1";
+export const component2 = () => {
+  console.log("component2 " + name);
+};
+
+component2();
